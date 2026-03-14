b13e25649cc6702a7fe1e6f4d770a0ff608e1a0a
Derek15235 <derekdjain@gmail.com>
2026-03-13 21:17:04 -0700

    Fixed up running state/ref

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  envDir: '..',
+  envDir: '.',
   test: {
     globals: true,
     environment: 'jsdom',
